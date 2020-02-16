60818acf1807387c9d15f3885ff12f389530f416
Andy Bettisworth <andy@accreu.com>
2020-02-16 17:07:53 -0600

    Improve styling and add onclick handlers

--- 1-vanilla/index.js
+++ 1-vanilla/index.js
@@ -1,3 +1,30 @@
+/**
+ * Define helpers
+ */
+
 function sayHello() {
   alert("Hello");
 }
+
+function addDot(colIndex) {
+  alert(colIndex);
+}
+
+/**
+ * Initialize game state
+ */
+
+function initGameState() {
+  /**
+   * Gotchas: I struggled to create a matrix object.
+   */
+  let state = {};
+  state["board"] = []
+
+  for (let i = 0; i < 12; i++) {
+    state["board"].push(new Array(12));
+  }
+  console.log(state["board"].length)
+}
+
+document.addEventListener("DOMContentLoaded", initGameState);
