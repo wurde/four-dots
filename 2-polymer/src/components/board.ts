2bef37dad967192382d654ddf5677425ea987157
Andy Bettisworth <andy@accreu.com>
2020-02-28 02:23:17 -0600

    Render header columns

--- 2-polymer/src/components/board.ts
+++ 2-polymer/src/components/board.ts
@@ -1,10 +1,39 @@
-import { LitElement, html, customElement } from "lit-element";
+import { LitElement, html, css, property, customElement } from "lit-element";
+
+import "./add-dot-btn";
 
 @customElement("four-dot-board")
 export class Board extends LitElement {
+  @property({ type: Array }) columns = Array.from(Array(12), (x,i) => i);
+
+  static get styles() {
+    return css`
+      main {
+        display: grid;
+        grid-template-columns: 10% auto 10%;
+      }
+    `
+  }
+
   render() {
     return html`
-      Board
-    `
+      <main>
+        <table>
+          <thead>
+            <tr>
+              ${this.columns.map(x => {
+                return html`
+                  <th>
+                    <add-dot-btn colIndex=${x}></add-dot-btn>
+                  </th>
+                `;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </main>
+    `;
   }
 }
