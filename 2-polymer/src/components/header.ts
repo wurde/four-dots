98d3e66f702958a2469347cd96537da5044ea736
Andy Bettisworth <andy@accreu.com>
2020-02-28 01:28:41 -0600

    Style the header

--- 2-polymer/src/components/header.ts
+++ 2-polymer/src/components/header.ts
@@ -1,7 +1,32 @@
-import { LitElement, html, customElement } from "lit-element";
+import { LitElement, html, css, customElement } from "lit-element";
 
 @customElement("four-dot-header")
 export class Header extends LitElement {
+  static get styles() {
+    return css`
+      header {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      button {
+        background-color: #fff;
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        outline: none;
+        cursor: pointer;
+      }
+      button:hover {
+        background-color: #ddd;
+        border: 1px solid #ddd;
+      }
+      button:active {
+        background-color: #999;
+        border: 1px solid #999;
+      }
+    `;
+  }
+
   restartGame() {
     alert("restart GAME")
   }
