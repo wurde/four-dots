bdfb59e80693283c3526c90290fb10266b791476
Andy Bettisworth <andy@accreu.com>
2020-02-28 01:23:39 -0600

    Add restartGame eventHandler

--- 2-polymer/src/components/header.ts
+++ 2-polymer/src/components/header.ts
@@ -2,10 +2,17 @@ import { LitElement, html, customElement } from "lit-element";
 
 @customElement("four-dot-header")
 export class Header extends LitElement {
+  restartGame() {
+    alert("restart GAME")
+  }
+
   render() {
     return html`
       <header>
         <h1>Four Dots</h1>
+        <button type="button" @click="${this.restartGame}">
+          Restart
+        </button>
       </header>
     `;
   }
