3e4bfec00e238fd27afdcce3578740012391f64e
Andy Bettisworth <andy@accreu.com>
2020-03-01 01:22:29 -0600

    Create board

--- 2-polymer/src/index.ts
+++ 2-polymer/src/index.ts
@@ -11,20 +11,30 @@ import "./components/board";
 
 @customElement("four-dots-app")
 export class FourDotsApp extends LitElement {
-  @property({ type: Array }) board = [];
-  @property({ type: String }) currentPlayer = "red";
+  @property({ type: Array }) board = this.createBoard();
+  @property({ type: String }) currentPlayer = "blue";
   @property({ type: String }) visibility = "hidden";
 
+  createBoard() {
+    const size = 12;
+    let board = [];
+    for (let i = 0; i < size; i++) {
+      board.push(new Array(size));
+    }
+    return board;
+  }
+
   restartGame() {
-    this.currentPlayer = "red";
+    this.currentPlayer = "blue";
     this.visibility = "hidden";
+    this.board = this.createBoard();
   }
 
   render() {
     return html`
       <four-dot-winneralert visibility="${this.visibility}"></four-dot-winneralert>
       <four-dot-header .restartGame="${this.restartGame}"></four-dot-header>
-      <four-dot-board></four-dot-board>
+      <four-dot-board board=${JSON.stringify(this.board)}></four-dot-board>
     `;
   }
 }
