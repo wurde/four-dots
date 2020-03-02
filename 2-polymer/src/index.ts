b6232aef3759bddfd1bccba6161d5479142f2eca
Andy Bettisworth <andy@accreu.com>
2020-03-01 21:18:02 -0600

    Check for winner

--- 2-polymer/src/index.ts
+++ 2-polymer/src/index.ts
@@ -42,6 +42,7 @@ export class FourDotsApp extends LitElement {
     if (rowIndex == 0) {
       this.columnFilled(colIndex);
     }
+    this.checkIfGameOver(this.board, rowIndex, colIndex, player);
   }
 
   changePlayer() {
@@ -50,6 +51,94 @@ export class FourDotsApp extends LitElement {
 
   columnFilled(colIndex) {
     this.actionbar[colIndex] = "disabled";
+  }
+
+  checkIfGameOver(board, rowIndex, colIndex, player) {
+    let count = 1;
+
+    // Check vertical
+    let counter = rowIndex + 1;
+    while (counter <= 11) {
+      let x = board[counter][colIndex];
+      if (!x || x != player) break;
+      count++;
+      counter++;
+    }
+    if (count == 4) {
+      this.rewardWinner(player);
+      return;
+    }
+
+    // Check horizontal
+    count = 1;
+    let leftCounter = colIndex - 1;
+    while (leftCounter >= 0) {
+      let x = board[rowIndex][leftCounter];
+      if (!x || x != player) break;
+      count++;
+      leftCounter--;
+    }
+    let rightCounter = colIndex + 1;
+    while (rightCounter <= 11) {
+      let x = board[rowIndex][rightCounter];
+      if (!x || x != player) break;
+      count++;
+      rightCounter++;
+    }
+    if (count == 4) {
+      this.rewardWinner(player);
+      return;
+    }
+
+    // Check diagonalLeft
+    count = 1;
+    leftCounter = colIndex - 1;
+    let topCounter = rowIndex - 1;
+    while (leftCounter >= 0 && topCounter >= 0) {
+      let x = board[topCounter][leftCounter];
+      if (!x || x != player) break;
+      count++;
+      leftCounter--;
+      topCounter--;
+    }
+    rightCounter = colIndex + 1;
+    let bottomCounter = rowIndex + 1;
+    while (rightCounter <= 11 && bottomCounter <= 11) {
+      let x = board[bottomCounter][rightCounter];
+      if (!x || x != player) break;
+      count++;
+      rightCounter++;
+      bottomCounter++;
+    }
+    if (count == 4) {
+      this.rewardWinner(player);
+      return;
+    }
+
+    // Check diagonalRight
+    count = 1;
+    leftCounter = colIndex - 1;
+    bottomCounter = rowIndex + 1;
+    while (leftCounter >= 0 && bottomCounter <= 11) {
+      let x = board[bottomCounter][leftCounter];
+      if (!x || x != player) break;
+      count++;
+      leftCounter--;
+      bottomCounter++;
+    }
+    topCounter = rowIndex - 1;
+    rightCounter = colIndex + 1;
+    while (rightCounter <= 11 && topCounter >= 0) {
+      let x = board[topCounter][rightCounter];
+      if (!x || x != player) break;
+      count++;
+      topCounter--;
+      rightCounter++;
+    }
+    if (count == 4) {
+      this.rewardWinner(player)
+      return;
+    }
   }
 
   render() {
